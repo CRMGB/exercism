88ad20517c8f080684000de34e6d92e01852620f
Cristian Martinez <cristianbisby@gmail.com>
2020-02-11 11:49:22 +0100

    Add files via upload

---
+++ high-scores/high_scores.py
@@ -0,0 +1,11 @@
+def latest(scores):
+    return scores[-1]
+    pass
+
+def personal_best(scores):
+    return max(scores)
+    pass
+
+def personal_top_three(scores):
+    return sorted((scores), reverse=True)[:3]
+    pass
