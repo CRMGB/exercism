9fa9853d53bdab1d960e99eb119fe25d387bb131
Cristian Martinez <cristianbisby@gmail.com>
2020-02-11 17:26:29 +0100

    Add files via upload

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -2,6 +2,9 @@
 def is_isogram(string):
     justChar = list(''.join(e for e in string if e.isalnum()).lower())
     noRepeat = dict.fromkeys(justChar)
-    if(len(justChar) != len(noRepeat)):
-        return False
-    return True
+    return len(justChar) == len(noRepeat)
+
+##Exercism solution:
+    # def is_isogram(string):
+    #     characters_lower = [c.lower() for c in string if c.isalpha()]
+    #     return len(set(characters_lower)) == len(characters_lower)
