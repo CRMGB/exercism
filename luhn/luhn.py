712d387a4f0f48514c4f704366ae73c632a81639
Cristian Martinez <cristianbisby@gmail.com>
2020-02-19 16:58:08 +0100

    Add files via upload

--- luhn/luhn.py
+++ luhn/luhn.py
@@ -17,11 +17,9 @@ class Luhn:
         #Undo the reverse list and then pass it to every over element, examp: [7, None, 5, None, 2, None, 6, None, 5, None, 1, None, 0, None, 3, None]
         self.result[::2] = newList[::-1][:num]
         self.result[1::2] = listNumbers[::2][::-1][:num]
-        print("self.result[1::2]; ", self.result)
         # We cobine the two lists again for the ones that are odd or uneven
         self.result.extend(newList[::-1][num:])
         self.result.extend(listNumbers[::2][::-1][num:])
-        print("self.result.extend; ", self.result)
             
     def valid(self):
         return True if sum(self.result) % 10 == 0 and self.result != [0] else False
